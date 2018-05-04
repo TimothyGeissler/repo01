9e9b202127fd539c3544fa6612dad4e3818442f8
timdg <timdg@DESKTOP-7FQSREE>
2018-05-04 19:59:23 +0200

    TestCommit with method

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -15,7 +15,8 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hi");
+        Class c = new Class();
+        System.out.println(c.Method());
     }
     
 }
